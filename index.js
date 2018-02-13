a4855efe5ab26bb2bf5db27a7f257a09bb0091e0
krreet <34994993+krreet@users.noreply.github.com>
2018-02-13 15:41:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const TeleBot = require('telebot');
 
 
+const http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Last, but not least, listen on port 8080
+// The environment variable PORT is automatically defined and equals to 8080
+server.listen(process.env.PORT, '0.0.0.0');
+
+
+
+
 let promo = "Please join our channel t.me/ultrapumpsignal and get FREE Airdrops!!. Do follow us at twitter https://goo.gl/ozfpqo";
 let bottypes = ["vvfast" ,"vfast", "fast", "med", "slow" , "vslow", "vvslow"];
 let vvfast_g  = ["officialios"];
