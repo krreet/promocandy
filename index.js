bd446cd81538024afac2587de41a5d496f8b4a2c
krreet <34994993+krreet@users.noreply.github.com>
2018-03-21 19:23:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ https://bitsong.io/start/MULH4ZJB` ;//cafwelbot 126690
 
 if(process.env.MSG){
 cafmsg1 = process.env.MSG;
-   // cafmsg1 = cafmsg1.substring(0, 145);
+    cafmsg1 = cafmsg1.substring(0, 143);
 }
 let cafmsg2 = `Make sure you Join and stay in our partner channel else you won't receive tokens. Leaving this channel will disqualify you from the AirDrop campaign!!!.
 t.me/ultrapumpsignal  . 
